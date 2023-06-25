4a1b8c75a74ec66db7edc301319a7beed85acde6
Bruce <recall4056@gmail.com>
2023-06-25 17:34:36 +0800

    fix: / router match bug

--- src/middlewares/routes/404.middleware.ts
+++ src/middlewares/routes/404.middleware.ts
@@ -3,8 +3,10 @@ import type { Request } from 'express'
 import type { BaseResponse } from '@/types'
 
 export const notFoundHandler = (request: Request, response: BaseResponse) => {
+  const { t } = request
+
   response.status(404).json({
     code: 404,
-    message: 'Not Found!'
+    message: t('Router.NotExist')
   })
 }
