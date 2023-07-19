c8792643f24a0a658e5241569d081da56d5361b0
Bruce Song <recall4056@gmail.com>
2023-07-19 08:47:12 +0800

    chore: update

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,17 +1,16 @@
 import type { Socket } from 'socket.io'
 
 export const demo = (socket: Socket) => {
-  socket.on('disconnect', () => {
-    console.log('Disconnected')
-  })
   socket.on('message', (data) => {
     console.log(data)
     socket.broadcast.emit('message', data)
   })
   socket.on('join', (data) => {
+    console.log(data)
     socket.broadcast.emit('message', data)
   })
   socket.on('leave', (data) => {
+    console.log(data)
     socket.broadcast.emit('message', data)
   })
 }
